8a6f3199a518cf90e8a704da8d186172845285eb
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-08-01 13:02:26 -0300

    Levers  Begining

---
+++ Projeto Dragao do Mar/Assets/Scripts/SwitchLogic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LeversWays
+{
+    FirstWay, SecondWay, ThirdWay
+}
+public class SwitchLogic : MonoBehaviour
+{
+    public GameObject[] levers;
+    public bool[] isUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//https://www.youtube.com/watch?v=bG-nZiJnx6s&list=PLrw9uFU6NhfOnEhqi7DRbx-eXiTrmbVGD
